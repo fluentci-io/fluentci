fb26ee4dd28d5c4b064b65da2a06cf56bb9a3e17
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-25 18:52:25 +0000

    feat: implement `upgrade` subcommand

---
+++ upgrade.ts
@@ -0,0 +1,20 @@
+async function upgrade() {
+  const command = new Deno.Command(Deno.execPath(), {
+    args: [
+      "install",
+      "-r",
+      "--import-map=https://deno.land/x/fluentci/import_map.json",
+      "https://deno.land/x/fluentci/main.ts",
+      "-n",
+      "fluentci",
+      "-f",
+    ],
+  });
+
+  const { stdout, stderr } = await command.output();
+
+  console.log(new TextDecoder().decode(stdout));
+  console.log(new TextDecoder().decode(stderr));
+}
+
+export default upgrade;
