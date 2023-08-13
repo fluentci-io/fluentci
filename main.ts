b21f0340215ac6994b184d0b7289574e79dbb895
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-13 17:50:47 +0000

    bump version code

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ import generateWorkflow from "./github.ts";
 export async function main() {
   await new Command()
     .name("fluentci")
-    .version("0.3.8")
+    .version("0.3.9")
     .description(
       `
       .
