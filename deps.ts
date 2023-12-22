4c8af206f7ea6b4b58f863219c8bd239e812d4bd
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-12-22 15:01:35 +0000

    feat: implement command line `login`

--- deps.ts
+++ deps.ts
@@ -11,3 +11,6 @@ export { z } from "https://deno.land/x/zod@v3.22.2/mod.ts";
 export { decompress } from "https://deno.land/x/zip@v1.2.5/mod.ts";
 export { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
 export { load } from "https://deno.land/std@0.205.0/dotenv/mod.ts";
+export { Secret } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/secret.ts";
+import dir from "https://deno.land/x/dir@1.5.2/mod.ts";
+export { dir };
