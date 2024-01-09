e1da36779bf1c21fba42d621e0d575ea43160760
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-09 11:06:46 +0000

    bump version code

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ import { brightGreen } from "./deps.ts";
 export async function main() {
   await new Command()
     .name("fluentci")
-    .version("0.10.0")
+    .version("0.10.1")
     .description(
       `
       .
