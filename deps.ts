da8996ab1eb60ef903e232b41c1f0a12bf82241c
Tsilavina Razafinirina <t.razafinirina@gmail.com>
2024-01-20 21:03:47 +0300

    deno fmt deps

--- deps.ts
+++ deps.ts
@@ -1,14 +1,14 @@
 import * as semver from "https://deno.land/std@0.212.0/semver/mod.ts";
 export { semver };
 export {
-  magenta,
+  bold,
+  brightGreen,
   brightMagenta,
   cyan,
-  bold,
-  green,
-  red,
-  brightGreen,
   gray,
+  green,
+  magenta,
+  red,
   yellow,
 } from "https://deno.land/std@0.192.0/fmt/colors.ts";
 export { z } from "https://deno.land/x/zod@v3.22.2/mod.ts";
@@ -17,22 +17,22 @@ export { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
 export { load } from "https://deno.land/std@0.205.0/dotenv/mod.ts";
 export { Secret } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/secret.ts";
 export {
-  prompt,
-  Input,
   Confirm,
+  Input,
+  prompt,
 } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
 import dir from "https://deno.land/x/dir@1.5.2/mod.ts";
 export { dir };
-export { walkSync, walk } from "https://deno.land/std@0.210.0/fs/walk.ts";
+export { walk, walkSync } from "https://deno.land/std@0.210.0/fs/walk.ts";
 export type { WalkEntry } from "https://deno.land/std@0.208.0/fs/walk.ts";
 export {
   BlobReader,
   BlobWriter,
   TextReader,
   TextWriter,
+  Uint8ArrayReader,
   ZipReader,
   ZipWriter,
-  Uint8ArrayReader,
 } from "https://deno.land/x/zipjs@v2.7.32/index.js";
 import introspect from "https://cdn.jsdelivr.net/gh/fluentci-io/daggerverse@main/deno-sdk/sdk/src/mod/introspect.ts";
 export { introspect };
