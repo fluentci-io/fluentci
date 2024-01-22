56f1929a94c2404f9779374b7b7241f7cf7cae90
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-22 18:29:58 +0000

    update repl.ts

--- src/cmd/repl.ts
+++ src/cmd/repl.ts
@@ -60,7 +60,7 @@ async function repl(
       "deno",
       "repl",
       "-A",
-      "--eval-file=https://cdn.jsdelivr.net/gh/fluentci-io/fluentci@7672799/src/prelude.ts",
+      "--eval-file=https://cdn.jsdelivr.net/gh/fluentci-io/fluentci@0c8a9aa/src/prelude.ts",
       ...args,
     ],
     stdout: "inherit",
@@ -75,7 +75,7 @@ async function repl(
       args: [
         "repl",
         "-A",
-        "--eval-file=https://cdn.jsdelivr.net/gh/fluentci-io/fluentci@7672799/src/prelude.ts",
+        "--eval-file=https://cdn.jsdelivr.net/gh/fluentci-io/fluentci@0c8a9aa/src/prelude.ts",
         ...args,
       ],
       env: {
