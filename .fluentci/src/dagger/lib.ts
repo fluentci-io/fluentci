58027d30b784c8eeea1f70b8f7384f5813700d7a
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-24 10:47:54 +0000

    ci: use fluentci flakestry function

--- .fluentci/src/dagger/lib.ts
+++ .fluentci/src/dagger/lib.ts
@@ -5,16 +5,49 @@ import Client, {
   SecretID,
 } from "../../deps.ts";
 
-export const getDirectory = (
+export const getDirectory = async (
   client: Client,
   src: string | Directory | undefined = "."
 ) => {
-  if (typeof src === "string" && src.startsWith("core.Directory")) {
-    return client.directory({
-      id: src as DirectoryID,
-    });
+  if (src instanceof Directory) {
+    return src;
   }
-  return src instanceof Directory ? src : client.host().directory(src);
+  if (typeof src === "string") {
+    try {
+      const directory = client.loadDirectoryFromID(src as DirectoryID);
+      await directory.id();
+      return directory;
+    } catch (_) {
+      return client.host
+        ? client.host().directory(src)
+        : client.currentModule().source().directory(src);
+    }
+  }
+  return client.host
+    ? client.host().directory(src)
+    : client.currentModule().source().directory(src);
+};
+
+export const getGithubToken = async (
+  client: Client,
+  token: string | Secret
+) => {
+  if (Deno.env.get("GH_TOKEN")) {
+    return client.setSecret("GH_TOKEN", Deno.env.get("GH_TOKEN")!);
+  }
+  if (token && typeof token === "string") {
+    try {
+      const secret = client.loadSecretFromID(token as SecretID);
+      await secret.id();
+      return secret;
+    } catch (_) {
+      return client.setSecret("GH_TOKEN", token);
+    }
+  }
+  if (token && token instanceof Secret) {
+    return token;
+  }
+  return undefined;
 };
 
 export const getDenoDeployToken = (client: Client, token?: string | Secret) => {
