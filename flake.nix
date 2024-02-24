4a423b9bb23971912908898a1e544f438d7e3002
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-24 11:40:35 +0000

    bump version code

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
 
         packages.default = pkgs.deno2nix.mkExecutable {
           pname = "fluentci";
-          version = "0.9.1";
+          version = "0.11.8";
 
           src = ./.;
           lockfile = "./deno.lock";
