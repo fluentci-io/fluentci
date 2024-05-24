49e9f062f8c53878b279913dd08088c4b99a194f
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-05-24 10:45:33 +0000

    ci: remove `.fluentci` directory

--- .fluentci/sdk/client.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLClient } from "../deps.ts";
-
-export function createGQLClient(port: number, token: string): GraphQLClient {
-  return new GraphQLClient(`http://127.0.0.1:${port}/query`, {
-    headers: {
-      Authorization: "Basic " + btoa(token + ":"),
-    },
-  });
-}
